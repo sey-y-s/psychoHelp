1167920804727a6211dd8bf81cdf067bcce5b77e
sey-y-s <seybayacoub@gmail.com>
2026-06-18 10:02:39 +0000

    initialisation du projet

---
+++ src/main/java/org/psychohelp/psychohelp/PsychoHelpApplication.java
@@ -0,0 +1,13 @@
+package org.psychohelp.psychohelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychoHelpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsychoHelpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/psychohelp/psychohelp/PsychoHelpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.psychohelp.psychohelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychoHelpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
