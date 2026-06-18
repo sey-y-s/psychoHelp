d555bf9d6253f297043d2971efe4cc78c3f932fe
med <seibacly@gmail.com>
2026-06-18 17:02:44 +0000

    etity utilisateur

--- src/main/java/org/psychohelp/psychohelp/entity/Utilisateur.java
+++ src/main/java/org/psychohelp/psychohelp/entity/Utilisateur.java
@@ -1,4 +1,34 @@
 package org.psychohelp.psychohelp.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+//specifier que ça correspond a une table de la bdd
+@Entity
+@Table(name = "utilisateurs")
+@Data //getters setters tostring...
+//le constructeur par defaut
+@NoArgsConstructor
+//constructeur avec les parametres
+@AllArgsConstructor
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Utilisateur {
+    @Id //designer comme clef primaire de la table
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto-incremente
+    private int id;
+
+    private String nom;
+    private String prenom;
+    private String mail;
+    private String telephone;
+    private String motDePasse;
+    private String role;
+    private Date date_creation;
+
+
+
+
 }
