ec4791fff87d6ef8feaf80f2f5f556b1611beca1
Dialla Coulibaly <coulibalydialla233@gmail.com>
2026-06-19 14:19:03 +0000

    ajout de l'implementation psy

--- src/main/java/org/psychohelp/psychohelp/service/PsyService.java
+++ src/main/java/org/psychohelp/psychohelp/service/PsyService.java
@@ -1,4 +1,23 @@
 package org.psychohelp.psychohelp.service;
 
+import org.psychohelp.psychohelp.entity.Psychologue;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface PsyService {
+
+    // ajout operation
+    Psychologue savePsychologue(Psychologue psychologue);
+
+    // lecture operation
+    List<Psychologue> PSYCHOLOGUEList();
+    Optional<Psychologue> GetPsychologueById(int PsychologueId);
+    List<Psychologue> UpdateEtat(int PsyId,boolean etat);
+
+
+    // modification operation
+    Psychologue updatePsychologue(Psychologue psychologue,int PsychologueId);
+
+
 }
