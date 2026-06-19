e3c45b8a29c3b65320e896993c19c3dcc1d42271
med <seibacly@gmail.com>
2026-06-19 15:37:50 +0000

    implemention des endpoints get, get/id, post (table utilisateur)

--- src/main/java/org/psychohelp/psychohelp/controller/UtilisateurController.java
+++ src/main/java/org/psychohelp/psychohelp/controller/UtilisateurController.java
@@ -1,6 +1,38 @@
 package org.psychohelp.psychohelp.controller;
 
+import org.psychohelp.psychohelp.entity.Utilisateur;
+import org.psychohelp.psychohelp.service.UtilisateurService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/utilisateurs")
 public class UtilisateurController {
+
+    UtilisateurService utilisateurService;
+
+    public UtilisateurController(UtilisateurService utlService){
+        this.utilisateurService = utlService;
+    }
+
+
+    @GetMapping(path = "list")
+    public List<Utilisateur> list(){
+        return utilisateurService.listeUtilisateur();
+    }
+
+    @GetMapping(path = "{id}")
+    public Utilisateur userById(@PathVariable int id){
+        return utilisateurService.utilisateurParId(id);
+    }
+
+    @PostMapping
+    public Utilisateur create(@RequestBody Utilisateur utl){
+        return utilisateurService.creer(utl);
+    }
+
+
 
 
 }
