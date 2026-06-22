da453933c551fbd40f37dda59f1908f3c1f68726
sey-y-s <seybayacoub@gmail.com>
2026-06-22 09:37:24 +0000

    ADD SeanceDto without really using it

--- src/main/java/org/psychohelp/psychohelp/dto/SeanceDTO.java
+++ src/main/java/org/psychohelp/psychohelp/dto/SeanceDTO.java
@@ -1,4 +1,25 @@
 package org.psychohelp.psychohelp.dto;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.psychohelp.psychohelp.enumeration.StatutRdvEnum;
+import org.psychohelp.psychohelp.entity.Citoyen;
+import org.psychohelp.psychohelp.entity.Creneau;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class SeanceDTO {
+    private int id;
+    private String nom;
+    private LocalDate date;
+    private LocalDate dateRdv;
+    private StatutRdvEnum statut;
+    private Citoyen citoyen;
+    private Creneau creneau;
 }
