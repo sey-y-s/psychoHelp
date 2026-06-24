9ee166732c1989db13f77b8bf41ebdb64fcdd9b0
MoussaTraore <moutra213@gmail.com>
2026-06-24 16:41:29 +0000

    cetaine lodif

--- src/main/java/org/psychohelp/psychohelp/service/SpecialiteService.java
+++ src/main/java/org/psychohelp/psychohelp/service/SpecialiteService.java
@@ -1,9 +1,12 @@
 package org.psychohelp.psychohelp.service;
 
+import org.psychohelp.psychohelp.dto.SpecialiteListeDto;
+import org.psychohelp.psychohelp.dto.UpdateSpecialiteDto;
 import org.psychohelp.psychohelp.entity.Psychologue;
 import org.psychohelp.psychohelp.entity.Specialite;
 import org.psychohelp.psychohelp.repository.SpecialiteRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,11 +14,14 @@ import java.util.List;
 public class SpecialiteService {
     @Autowired
     private SpecialiteRepo specialiteRepo;
-    public Specialite ajouter(Specialite specialite){
-        return specialiteRepo.save(specialite);
+    public void ajouter(UpdateSpecialiteDto updateSpecialiteDto) {
+        Specialite specialite=new Specialite();
+        specialite.setNom(updateSpecialiteDto.getNom());
+
+        specialiteRepo.save(specialite);
     }
     public List<Specialite> ListeSpecialite(){
-        return specialiteRepo.findAll();
+        return specialiteRepo.findAll(Sort.by(Sort.Direction.ASC, "id"));
     }
     public Specialite getSpecialite(int id){
         return specialiteRepo.findById(id).orElse(null);
@@ -25,5 +31,12 @@ public class SpecialiteService {
     }
     public List<Psychologue>getSpecialiteIsPsycholoque(int id){
         return specialiteRepo.getSpecialiteIsPsychologue(id);
+    }
+    public SpecialiteListeDto updateSpecialite(int id,UpdateSpecialiteDto updateSpecialiteDto){
+        Specialite specialite=specialiteRepo.findById(id).orElseThrow(()->new RuntimeException("cette specialite est introuvable"));
+        specialite.setNom(updateSpecialiteDto.getNom());
+        Specialite specialitemodif =specialiteRepo.save(specialite);
+        return new SpecialiteListeDto(specialitemodif.getId(),specialitemodif.getNom());
+
     }
 }
