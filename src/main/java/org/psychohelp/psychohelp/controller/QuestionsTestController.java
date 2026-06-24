7c99bcb1cf577fc36344c85b01ede2832a2d7c5a
DIARRA Adama <ad5299019@gmail.com>
2026-06-24 10:44:46 +0000

    Adama_modif_swagger

--- src/main/java/org/psychohelp/psychohelp/controller/QuestionsTestController.java
+++ src/main/java/org/psychohelp/psychohelp/controller/QuestionsTestController.java
@@ -1,5 +1,8 @@
 package org.psychohelp.psychohelp.controller;
 
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.psychohelp.psychohelp.entity.QuestionsTest;
 import org.psychohelp.psychohelp.service.QuestionsTestService;
@@ -8,34 +11,65 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
-@RequestMapping("/questions")
-@AllArgsConstructor
-public class QuestionsTestController {
-    private final QuestionsTestService questionsTestService ;
 
+@RestController
+@RequestMapping("/api/questions")
+@AllArgsConstructor
+
+@Tag(
+        name = "Questions des tests",
+        description = "Cette API permet de gérer les questions associées aux tests psychologiques. Elle permet d'ajouter de nouvelles questions, consulter les questions existantes, modifier leurs informations et les supprimer."
+)
+public class QuestionsTestController {
+
+
+    private final QuestionsTestService questionsTestService;
+
+
+
+    @Operation(
+            summary = "Récupérer toutes les questions",
+            description = "Retourne la liste de toutes les questions des tests psychologiques"
+    )
     @GetMapping
     public List<QuestionsTest> getAllQuestions(){
+
         return questionsTestService.getallQuestions();
     }
 
 
+
+    @Operation(
+            summary = "Récupérer une question par ID",
+            description = "Retourne une question spécifique selon son identifiant"
+    )
     @GetMapping("/{id}")
     public Optional<QuestionsTest> getTestById(@PathVariable int id) {
 
         return questionsTestService.getQuestionsById(id);
     }
 
+
+
+    @Operation(
+            summary = "Créer une question",
+            description = "Ajoute une nouvelle question associée à un test psychologique"
+    )
     @PostMapping
     public QuestionsTest saveQuestions(
-            @RequestBody QuestionsTest Questions){
+            @RequestBody QuestionsTest questions){
 
-        return questionsTestService.saveQuestions(Questions);
+        return questionsTestService.saveQuestions(questions);
 
     }
 
 
 
+
+    @Operation(
+            summary = "Modifier une question",
+            description = "Met à jour une question existante"
+    )
     @PutMapping("/{id}")
     public QuestionsTest updateQuestions(
             @PathVariable int id,
@@ -48,6 +82,11 @@ public class QuestionsTestController {
 
 
 
+
+    @Operation(
+            summary = "Supprimer une question",
+            description = "Supprime une question selon son identifiant"
+    )
     @DeleteMapping("/{id}")
     public void deleteQuestions(
             @PathVariable int id){
--- src/main/java/org/psychohelp/psychohelp/controller/TestController.java
+++ src/main/java/org/psychohelp/psychohelp/controller/TestController.java
@@ -1,6 +1,8 @@
 package org.psychohelp.psychohelp.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.psychohelp.psychohelp.entity.Test;
 import org.psychohelp.psychohelp.service.TestService;
@@ -10,21 +12,32 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/tests")
+@RequestMapping("/api/tests")
 @AllArgsConstructor
+
+@Tag(
+        name = "Tests psychologiques",
+        description = "Cette API permet de créer, consulter, modifier et supprimer les tests psychologiques proposés aux citoyens. Elle permet aussi de récupérer les informations d'un test ainsi que ses différentes données associées."
+)
 public class TestController {
 
 
     private final TestService testService;
 
-
+    @Operation(
+            summary = "Récupérer tous les tests",
+            description = "Retourne la liste de tous les tests psychologiques disponibles"
+    )
     @GetMapping
     public List<Test> getAllTests() {
 
         return testService.getAllTests();
     }
 
-
+    @Operation(
+            summary = "Récupérer un test par son identifiant",
+            description = "Retourne un test psychologique selon son ID"
+    )
     @GetMapping("/{id}")
     public Optional<Test> getTestById(@PathVariable int id) {
 
@@ -32,6 +45,11 @@ public class TestController {
     }
 
 
+
+    @Operation(
+            summary = "Créer un nouveau test",
+            description = "Ajoute un nouveau test psychologique dans la base de données"
+    )
     @PostMapping
     public Test saveTest(@RequestBody Test test) {
 
@@ -39,6 +57,10 @@ public class TestController {
     }
 
 
+    @Operation(
+            summary = "Modifier un test",
+            description = "Met à jour les informations d'un test existant"
+    )
     @PutMapping("/{id}")
     public Test updateTest(
             @PathVariable int id,
@@ -50,6 +72,10 @@ public class TestController {
     }
 
 
+    @Operation(
+            summary = "Supprimer un test",
+            description = "Supprime un test psychologique selon son identifiant"
+    )
     @DeleteMapping("/{id}")
     public void deleteTest(@PathVariable int id) {
 
