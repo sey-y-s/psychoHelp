9656170d47b5d85045f9552f43c74ad1ebcf0ccd
med <seibacly@gmail.com>
2026-06-25 14:13:19 +0000

    conseil (°_°)

--- src/main/java/org/psychohelp/psychohelp/controller/UtilisateurController.java
+++ src/main/java/org/psychohelp/psychohelp/controller/UtilisateurController.java
@@ -1,5 +1,7 @@
 package org.psychohelp.psychohelp.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.psychohelp.psychohelp.entity.Utilisateur;
 import org.psychohelp.psychohelp.serviceImpl.UtilisateurServiceImpl;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +10,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/utilisateurs")
+@Tag(
+        name = "Utilisateur",
+        description = "Les opération sur l'utilisateur"
+)
 public class UtilisateurController {
 
     UtilisateurServiceImpl utilisateurService;
@@ -17,34 +23,50 @@ public class UtilisateurController {
     }
 
 
+    @Operation(
+            summary = "Liste",
+            description = "voir la liste des utilisateurs"
+    )
     @GetMapping(path = "list")
     public List<Utilisateur> list(){
         return utilisateurService.listeUtilisateur();
     }
 
 
+    @Operation(
+            summary = "Un utilisateur",
+            description = "Recuperer un utilisateur par son identifiant"
+    )
     @GetMapping(path = "{id}")
     public Utilisateur userById(@PathVariable int id){
         return utilisateurService.utilisateurParId(id);
     }
 
+    @Operation(
+            summary = "Insertion",
+            description = "Inserer un nouvel utilisateur"
+    )
     @PostMapping(path = "post")
     public Utilisateur create(@RequestBody Utilisateur utl){
         return utilisateurService.creer(utl);
     }
 
-    @GetMapping(path = "/test")
-    public String test() {
-        return "test";
-    }
 
 
+    @Operation(
+            summary = "Modification",
+            description = "Modifier un utilisateur à partir de l'id"
+    )
     @PutMapping(path = "update/{id}")
     public Utilisateur update(@PathVariable int id, @RequestBody Utilisateur utl){
         return utilisateurService.modifier(id, utl);
     }
 
 
+    @Operation(
+            summary = "Suppression",
+            description = "Supprimer un utilisateur"
+    )
     @DeleteMapping(path = "delete/{id}")
     public void delete(@PathVariable int id){
         utilisateurService.supUtilisateur(id);
