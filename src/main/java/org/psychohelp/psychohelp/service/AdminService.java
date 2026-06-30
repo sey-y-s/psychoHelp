e2e3aa27d8868bb3ce4f11d5555af2bf126b7a93
Kadidiatou <okadidiatou78@gmail.com>
2026-06-30 14:52:03 +0000

    ajout des modification

--- src/main/java/org/psychohelp/psychohelp/service/AdminService.java
+++ src/main/java/org/psychohelp/psychohelp/service/AdminService.java
@@ -1,11 +1,12 @@
 package org.psychohelp.psychohelp.service;
 
 import org.psychohelp.psychohelp.dto.AdminDTO;
-import org.psychohelp.psychohelp.entity.Admin;
-import org.psychohelp.psychohelp.entity.Conseil;
-import org.psychohelp.psychohelp.entity.Psychologue;
+import org.psychohelp.psychohelp.dto.QuestionsTestsDTO;
+import org.psychohelp.psychohelp.dto.TestDTO;
+import org.psychohelp.psychohelp.entity.*;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AdminService {
 
@@ -26,5 +27,25 @@ public interface AdminService {
     Psychologue validerInscriptionPsy(Integer id);
 
     Psychologue annulerInscriptionPsy(Integer id);
+
+    List<Test> getAllTests();
+
+    Optional<Test> getTestById(Integer id);
+
+    Test saveTest(TestDTO testDTO);
+
+    Test updateTest(Integer id, TestDTO testDTO);
+
+    void deleteTest(Integer id);
+
+    List<QuestionsTest> getAllQuestions();
+
+    QuestionsTest getQuestionById(Integer id);
+
+    QuestionsTest saveQuestion(QuestionsTestsDTO question);
+
+    QuestionsTest updateQuestion(Integer id, QuestionsTestsDTO question);
+
+    void deleteQuestion(Integer id);
 
 }
