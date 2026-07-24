8a383e957a24c24233349f1be4543072e9a0a660
C.Moh <125911644+MohamedCoulibaly16@users.noreply.github.com>
2026-07-24 15:05:20 +0000

    Remove unused JsonIgnoreProperties import

--- src/main/java/org/psychohelp/psychohelp/entity/Psychologue.java
+++ src/main/java/org/psychohelp/psychohelp/entity/Psychologue.java
@@ -1,6 +1,5 @@
 package org.psychohelp.psychohelp.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
